87dadce0d758578975f2d92897f69bba9274a6a1
Alexander Daniel <alexander.daniel@gmx.at>
2013-08-01 20:03:13 +0200

    coding dojo

--- src/main/scala/dojo/PotterKata.scala
+++ src/main/scala/dojo/PotterKata.scala
@@ -6,21 +6,22 @@ package dojo
 object PotterKata {
 
   def distinct(input: Seq[Int]): Seq[Seq[Int]] = {
-     if(input.isEmpty){
-       return Nil
-     }
-     val unique = input.distinct
-     val remainder = input.diff(unique)
-    (unique :: distinct(remainder).toList).toSeq
+    if (input.isEmpty) {
+      return Nil
+    }
+    val unique = input.distinct
+    val remainder = input.diff(unique)
+    unique +: distinct(remainder)
   }
 
   def compute(bookNumbers: Seq[Int]): Double = {
-    (for(e <- distinct(bookNumbers)) yield
-    getPriceForSet(e)).sum
+    (for (e <- distinct(bookNumbers)) yield
+      getPriceForSet(e)).sum
   }
 
   def getPriceForSet(bookNumbers: Seq[Int]): Double = {
-    val setSize = bookNumbers.toSet.size
+    require(bookNumbers.size == bookNumbers.distinct.size)
+    val setSize = bookNumbers.size
     val discount = setSize match {
       case 5 => 0.75
       case 4 => 0.8
@@ -28,7 +29,6 @@ object PotterKata {
       case 2 => 0.95
       case _ => 1.0
     }
-    val withoutDiscount = bookNumbers.size-setSize
-    (setSize * 8 * discount) + (withoutDiscount * 8)
+    setSize * 8 * discount
   }
 }
