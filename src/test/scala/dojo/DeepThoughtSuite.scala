554d5244bc09696de0de3c11e810bd9d4c177258
Alexander Daniel <alexander.daniel@gmx.at>
2013-08-01 20:24:20 +0200

    coding dojo

--- src/test/scala/dojo/DeepThoughtSuite.scala
+++ src/test/scala/dojo/DeepThoughtSuite.scala
@@ -61,6 +61,20 @@ class PotterKataSuite extends FunSuite {
   test("one set of four books and one set of two books") {
     assert(8 * 4 * 0.8 + 8 * 2 * 0.95 === compute(Seq(0, 0, 1, 2, 2, 3)))
   }
-//  test("one set of four books and one set of two books") {
-//    assert(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
+  test("one set of five books and one set of one book") {
+    assert(8 + (8 * 5 * 0.75) === compute(Seq(0, 1, 1, 2, 3, 4)))
+  }
+
+//  test("two sets of four books") {
+//    assert(2 * (8 * 4 * 0.8) === compute(Seq(0, 0, 1, 1, 2, 2, 3, 4)))
+//  }
+
+
+//  assert_equal(2 * (8 * 4 * 0.8), price([0, 0, 1, 1, 2, 2, 3, 4]))
+//  assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
+//    price([0, 0, 0, 0, 0,
+//  1, 1, 1, 1, 1,
+//  2, 2, 2, 2,
+//  3, 3, 3, 3, 3,
+//  4, 4, 4, 4]))
 }
